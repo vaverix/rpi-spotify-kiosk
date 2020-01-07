6dc57fdec0ebf235e0b85a1dadddfefaa7bc3008
vaverix <vaverix@o2.pl>
2020-01-07 08:13:17 +0100

    update

--- static/php/update.php
+++ static/php/update.php
@@ -1,6 +1,5 @@
 <?php
 error_reporting(E_ALL);
 $output = shell_exec('sudo sh /var/www/html/dist/php/update.sh');
-file_put_contents("/var/log/lighttpd/access.log", $output, FILE_APPEND | LOCK_E$
 echo "<pre>$output</pre>";
 ?>
