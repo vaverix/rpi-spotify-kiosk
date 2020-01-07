78977b157c3a103d42dd8b4a2a5e4f1e2e70e3c2
vaverix <vaverix@o2.pl>
2020-01-07 07:04:51 +0100

    update

--- dist/php/volume.php
+++ dist/php/volume.php
@@ -2,8 +2,11 @@
 error_reporting(E_ALL);
 $volume = isset($_GET['percent']) ? $_GET['percent'] : '';
 if (empty($volume)) {
-    $output = system('sudo vol');
+    $command = 'sudo vol';
+    $output = shell_exec($command);
 } else {
-    $output = system('sudo vol '.$volume);
+    $command = 'sudo vol '.$volume;
+    $output = shell_exec($command);
 }
+echo("command: $command<br>output: $output");
 ?>
