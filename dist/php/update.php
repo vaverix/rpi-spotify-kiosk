433d5cca0820034c9fd76bd30278a68f92fe7cd3
vaverix <vaverix@o2.pl>
2020-01-07 06:18:38 +0100

    update

--- dist/php/update.php
+++ dist/php/update.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(E_ALL);
-$output = echo shell_exec('sh /var/www/html/dist/php/update.sh');
+$output = shell_exec('sh /var/www/html/dist/php/update.sh');
 echo "<pre>$output</pre>";
 ?>
